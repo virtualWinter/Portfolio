0858bf9ec25ea62c740c712271b1134dc8869534
virtualWinter <admin@arsn.cc>
2024-12-09 20:39:52 +0200

    whoops, forgot to add toaster to root layout, my fault

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import {ThemeProvider} from '@/components/providers/theme-provider'
 import React from "react";
 import {Analytics} from "@vercel/analytics/react"
+import {Toaster} from "@/components/ui/toaster";
 
 export default function RootLayout({
                                        children,
@@ -14,6 +15,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} forcedTheme="dark">
             {children}
         </ThemeProvider>
+        <Toaster />
         <Analytics/>
         </body>
         </html>
