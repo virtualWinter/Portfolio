88eaac0616808c1cc8b5cc6f6807fb94fe801045
virtualWinter <admin@arsn.cc>
2024-12-09 13:31:18 +0200

    vercel analitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import {ThemeProvider} from '@/components/providers/theme-provider'
 import React from "react";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function RootLayout({
                                        children,
@@ -13,6 +14,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} forcedTheme="dark">
             {children}
         </ThemeProvider>
+        <Analytics />
         </body>
         </html>
     )
